1d084c43439fca59e009c8df2593d1689163cb5b
petrero <petrero@gmail.com>
2014-04-05 18:39:39 +0300

    Setup intervention image class

--- app/config/app.php
+++ app/config/app.php
@@ -106,7 +106,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-                'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider'
+                'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+                'Intervention\Image\ImageServiceProvider'
 	),
 
 	/*
@@ -172,6 +173,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+                'Image'           => 'Intervention\Image\Facades\Image'
 
 	),
 
