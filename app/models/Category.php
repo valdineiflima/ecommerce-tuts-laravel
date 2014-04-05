fb718261d3b932574390f5b0089a172f15cf7d50
petrero <petrero@gmail.com>
2014-04-05 16:11:18 +0300

    create Category model

---
+++ app/models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Category extends Eloquent{
+    protected $fillable = array('name');
+    
+    public static $rules = array(
+        'name' => 'required|min:3'
+    );
+}
