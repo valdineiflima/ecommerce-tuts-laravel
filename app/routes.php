58544afd843fd3c60de944fb8a5d591bad03f7fc
petrero <petrero@gmail.com>
2014-04-07 22:55:01 +0300

    users controller and users routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,4 @@ Route::get('/', array('uses' => 'StoreController@getIndex'));
 Route::controller('admin/categories', 'CategoriesController');
 Route::controller('admin/products', 'ProductsController');
 Route::controller('store', 'StoreController');
+Route::controller('users', 'UsersController');
