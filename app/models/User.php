86668014681a55636f4da58518dc7c2c2f113cd3
petrero <petrero@gmail.com>
2014-04-07 22:49:12 +0300

    generated laravel User model

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,50 @@
 <?php
 
-class User extends Eloquent{
-    
-}
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
 
+class User extends Eloquent implements UserInterface, RemindableInterface {
+
+    /**
+     * The database table used by the model
+     * 
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     * 
+     * @var array
+     */
+    protected $hidden = array('password');
+    
+
+    /**
+     * Get the unique identifier for the user
+     * 
+     * @return mixed
+     */
+    public function getAuthIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the password for the user.
+     * 
+     * @return string
+     */
+    public function getAuthPassword() {
+        return $this->password;
+    }
+
+    /**
+     * Get the email address where password reminders are sent.
+     * 
+     * @return string
+     */
+    public function getReminderEmail() {
+        return $this->email;
+    }
+
+}
